ab16a1aa4672f805380f1fc56e637a51f88313f6
Ivan Lin <35771002+ivanlinhf@users.noreply.github.com>
2024-11-26 11:13:43 +0800

    Add quasar

--- src/dalaran-web/src/main.ts
+++ src/dalaran-web/src/main.ts
@@ -1,9 +1,20 @@
+import { Quasar } from 'quasar'
 import { createApp } from 'vue'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(Quasar, {
+  plugins: {},
+})
 
 app.mount('#app')
